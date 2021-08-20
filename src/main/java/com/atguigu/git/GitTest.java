146dbe543f452a8f9f3523e75a7c89ff91c814db
gao <1208260754@qq.com>
2021-08-20 21:02:35 +0800

    github commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("gitee test!");
           System.out.println("gitee test222!");
+          System.out.println("gitee push test!");
     }
 }
